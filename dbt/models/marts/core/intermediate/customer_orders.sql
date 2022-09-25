86427046d2b488f7255c21889f1ea0af48405972
fdelletter@gmail.com <fdelletter@gmail.com>
2022-09-25 22:20:48 +0000

    test c

--- dbt/models/marts/core/intermediate/customer_orders.sql
+++ dbt/models/marts/core/intermediate/customer_orders.sql
@@ -17,8 +17,7 @@ final as (
         customer_id,
 
         min(order_date) as first_order,
-        max(order_date) as most_recent_order,
-        count(order_id) as number_of_orders
+        max(order_date) as most_recent_order
     from orders
 
     group by 1
