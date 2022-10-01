ff9af3121b2d939656947b2984a78372e9a9f99b
fdelletter@gmail.com <fdelletter@gmail.com>
2022-10-01 16:06:29 +0000

    x

--- dbt/models/staging/stg_orders.sql
+++ dbt/models/staging/stg_orders.sql
@@ -11,7 +11,7 @@ with source as (
     Normally we would select from the table here, but we are using seeds to load
     our data in this project
     #}
-    select * from {{ ref('raw_orders') }}
+    select name from {{ ref('raw_orders') }}
 
 ),
 
